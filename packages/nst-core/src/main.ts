9de60b9778c06c8cb09dd2b93add2daf80813b0e
pratiqdev <pratiqdev@gmail.com>
2023-07-26 11:29:16 -0400

    Testing types

--- packages/nst-core/src/main.ts
+++ packages/nst-core/src/main.ts
@@ -295,7 +295,7 @@ export default createStore;
 
 
 const nst1 = createStore({
-    greetings: 'hello'
+    greetings: 'hello' 
 })
 
 //&                                                                                                                     
